1950b23acc706c286f0f95aaf3342d4960a2aeb8
Rohu111 <rs28645790@gmail.com>
2026-06-05 18:28:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
-st.title("Employee Search System")
+employ = {
+    "101": {
+        "name": "Rohan",
+        "designation": "CEO"
+    },
+    "102": {
+        "name": "Yoga",
+        "designation": "Co-Founder"
+    },
+    "103": {
+        "name": "Lahari",
+        "designation": "Head"
+    }
+}
 
-ID = st.text_input("Enter Employee ID: ")
+st.title("Employee Search System")
+ID = st.number_input("Enter Employee ID: ")
 st.button("Search")
+
+if ID in employ:
+  print("\nEmployee Found!")
+  print("ID: ",ID)
+  print("Name: ",employ[ID]["name"])
+  print("Designation: ",employ[ID]["designation"])
